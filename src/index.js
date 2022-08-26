17f56e3aa21f8b401517660ac176079e0971327d
Maureen Njihia <maureennjihia468@gmail.com>
2022-08-26 17:54:31 +0300

    Fetch and render character names

--- src/index.js
+++ src/index.js
@@ -1,1 +1,25 @@
 // Your code here
+const baseUrl = " http://localhost:3000";
+//Fetch Character Names
+function fetchCharacterNames () {
+    return fetch(`${baseUrl}/characters`)
+    .then(response => response.json())
+}
+
+function renderCharacterNames(character) {
+    const characterBar = document.getElementById("character-bar");
+    const span = document.createElement("span");
+    span.innerHTML = character.name;
+    characterBar.appendChild(span);
+    console.log(characterBar);
+};
+
+fetchCharacterNames().then(characters => {
+    characters.forEach(character => {
+        renderCharacterNames(character);
+    })
+})
+
+document.addEventListener("DOMContentLoaded", function () {
+    fetchCharacterNames();
+})
