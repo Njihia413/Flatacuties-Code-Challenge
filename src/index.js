dceb3660e8dad9ef422797d893b2bab0ce6665ef
Maureen Njihia <maureennjihia468@gmail.com>
2022-08-26 21:31:33 +0300

    Fetch and render a particular character's details

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 // Your code here
-const baseUrl = " http://localhost:3000";
+const baseUrl = " http://localhost:3000/characters";
 //Fetch Character Names
 function fetchCharacterNames () {
-    return fetch(`${baseUrl}/characters`)
+    return fetch(baseUrl)
     .then(response => response.json())
 }
 
@@ -11,7 +11,8 @@ function renderCharacterNames(character) {
     const span = document.createElement("span");
     span.innerHTML = character.name;
     characterBar.appendChild(span);
-    console.log(characterBar);
+    span.dataset.id = character.id;
+    span.addEventListener("click", onSpanCharacterClick);
 };
 
 fetchCharacterNames().then(characters => {
@@ -20,6 +21,38 @@ fetchCharacterNames().then(characters => {
     })
 })
 
+
+
+//Fetching character details
+function fetchCharacterDetails (id) {
+    return fetch(baseUrl + `/${id}`)
+    .then(response => response.json())
+}
+
+
+function onSpanCharacterClick (event) {
+    fetchCharacterDetails(event.target.dataset.id)
+    .then(renderCharacterDetails);
+}
+
+function renderCharacterDetails(character) {
+    const characterInfo = document.getElementById("detailed-info");
+    const charName = document.getElementById("name");
+    charName.innerText = character.name
+
+    const charImg = document.getElementById("image");
+    charImg.src = character.image
+
+    const charVotes = document.getElementById("vote-count");
+    charVotes.innerText = character.votes
+
+    //characterInfo.appendChild(charName, charImg, charVotes);
+}
+
+
+
+
 document.addEventListener("DOMContentLoaded", function () {
     fetchCharacterNames();
+    fetchCharacterDetails();
 })
